9a9ea50f7cf2de37984599b2ef924e778f39d474
hazubeep <hazbyarp7@gmail.com>
2024-09-30 16:08:07 +0700

    rest parameter

--- js/script.js
+++ js/script.js
@@ -1,6 +1,3 @@
-for (let i = 0 ; i < 100; i++) {
-  for (let j = 0; j < 10 ; j++) {
-    console.log(`${i} - ${j}`);
-  }
+for (let i = 0 ; i < 100; i++) { for (let j = 0; j < 10 ; j++) {console.log(`${i} - ${j}`); }
 }
 
